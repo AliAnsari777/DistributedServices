35ab34dda59f78af6707129524beff8cb781792f
AliAnsari777 <alidk2013@gmail.com>
2023-05-12 00:50:35 -0700

    notification mircorservice created with its configuration and basic dependencies

---
+++ notification/src/main/java/com/aliacoding/notification/NotificationApplication.java
@@ -0,0 +1,17 @@
+package com.aliacoding.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(
+        basePackages = "com.aliacoding.clients"
+)
+public class NotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+}
