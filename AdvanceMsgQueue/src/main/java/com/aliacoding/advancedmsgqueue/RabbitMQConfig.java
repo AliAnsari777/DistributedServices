5a4bd8f615a4ac891958b34071209eea67fffd15
AliAnsari777 <alidk2013@gmail.com>
2023-05-22 23:23:20 -0700

    config file added to RabbitMQ with a listener, message converter and RabbitTemplate to send messages

---
+++ AdvanceMsgQueue/src/main/java/com/aliacoding/advancedmsgqueue/RabbitMQConfig.java
@@ -0,0 +1,41 @@
+package com.aliacoding.advancedmsgqueue;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfig {
+
+    private final ConnectionFactory connectionFactory;
+
+    // this method enable us to send messages
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    // this method enable us to receive messages. it is a listener
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(){
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+    // this method will convert object to JSON, so we can send them to a queue
+    @Bean
+    public MessageConverter jacksonConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+}
