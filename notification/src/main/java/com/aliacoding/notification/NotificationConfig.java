561b8b7ae94dc2e7c9dceb934425f172b5a26772
AliAnsari777 <alidk2013@gmail.com>
2023-05-22 23:54:53 -0700

    I created the Exchange, Queue, and Binding in the notificationConfig

--- notification/src/main/java/com/aliacoding/notification/NotificationConfig.java
+++ notification/src/main/java/com/aliacoding/notification/NotificationConfig.java
@@ -1,7 +1,12 @@
 package com.aliacoding.notification;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -17,4 +22,22 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKeys;
+
+    @Bean
+    public TopicExchange internalTopic() {
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopic())
+                .with(this.internalNotificationRoutingKeys);
+    }
 }
